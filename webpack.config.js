2a20771dc27f7cf44649a19d0f04d8adc41abd1b
Adrian Guerrero <adrianjguerrero87@gmail.com>
2019-05-19 21:53:20 -0400

    postcss autoprefixer!! yes!!

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require('path');
 const MiniCssExtractPlugin = require ('mini-css-extract-plugin');
 const OptimizeCssAsset = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const webpack = require("webpack");
+const autoprefixer = require("autoprefixer");
 
 module.exports = {
   entry: './js/main.js',
@@ -18,10 +20,17 @@ module.exports = {
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
-      { test: /\.scss$/, use:[MiniCssExtractPlugin.loader,'css-loader','sass-loader']},
+      { test: /\.scss$/, use:[MiniCssExtractPlugin.loader,'css-loader','postcss-loader','sass-loader']},
       {test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
       loader: 'url-loader?limit=1000000' }
     ]
   },
-  plugins : [new MiniCssExtractPlugin() ]
+  plugins : [new MiniCssExtractPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+          postcss: [
+              autoprefixer()
+          ]
+      }
+  }) ]
 };
