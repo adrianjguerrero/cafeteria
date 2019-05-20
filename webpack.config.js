6109a989a6c1518bb29d037b1d98e21acd6fa65d
Adrian Guerrero <adrianjguerrero87@gmail.com>
2019-05-19 21:41:43 -0400

    creador del archivo css y minifcador para el mismo, el terser es necesario porq sobreescribimos el minifier de webpack para js lol

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const MiniCssExtractPlugin = require ('mini-css-extract-plugin');
+const OptimizeCssAsset = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: './js/main.js',
@@ -6,12 +9,19 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  optimization:{
+    minimizer:[
+      new OptimizeCssAsset(),
+      new TerserPlugin()
+    ]
+  },
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
-      { test: /\.scss$/, use:['style-loader','css-loader','sass-loader']},
+      { test: /\.scss$/, use:[MiniCssExtractPlugin.loader,'css-loader','sass-loader']},
       {test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
       loader: 'url-loader?limit=1000000' }
     ]
-  }
+  },
+  plugins : [new MiniCssExtractPlugin() ]
 };
